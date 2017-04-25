daf16c07332b43a5b59728958bd072e1a1a825e7
lthanhbinh <lthanhbinh@tma.com.vn>
2017-04-25 13:49:04 +0700

    single blog

---
+++ BE/wp-content/themes/nexmo/sidebar-blog.php
@@ -0,0 +1,127 @@
+<div class="Blog-sidebarContainer">
+   <div class="Blog-sidebar">
+      <!-- Search Widget -->
+      <div class="widget widget_search">
+         <form class="search ng-pristine ng-valid" method="get" action="/" role="search">
+            <input class="search-input" type="search" name="s" placeholder="Search">
+            <button class="search-submit" type="submit" role="button">Go</button>
+         </form>
+      </div>
+      <div class="Blog-sidebarContent">
+         <!-- Categories Widget -->
+         <div class="widget widget_categories">
+            <h2 class="widgettitle">Categories</h2>
+            <ul>
+               <li class="cat-item cat-item-239"><a href="https://www.nexmo.com/blog/category/award/">Award</a></li>
+               <li class="cat-item cat-item-44"><a href="https://www.nexmo.com/blog/category/carriers/">Carriers</a></li>
+               <li class="cat-item cat-item-53"><a href="https://www.nexmo.com/blog/category/chat-app/">Chat App</a></li>
+               <li class="cat-item cat-item-41"><a href="https://www.nexmo.com/blog/category/cloud-communications/">Cloud Communications</a></li>
+               <li class="cat-item cat-item-484"><a href="https://www.nexmo.com/blog/category/customer-support/">Customer Support</a></li>
+               <li class="cat-item cat-item-56"><a href="https://www.nexmo.com/blog/category/customers/">Customers</a></li>
+               <li class="cat-item cat-item-458"><a href="https://www.nexmo.com/blog/category/dashboard/">Dashboard</a></li>
+               <li class="cat-item cat-item-164"><a href="https://www.nexmo.com/blog/category/developer-challenge/">Developer Challenge</a></li>
+               <li class="cat-item cat-item-47"><a href="https://www.nexmo.com/blog/category/developers-corner/">Developer's Corner</a></li>
+               <li class="cat-item cat-item-59"><a href="https://www.nexmo.com/blog/category/developers-2/">Developers</a></li>
+               <li class="cat-item cat-item-329"><a href="https://www.nexmo.com/blog/category/disruption-discussion/">Disruption Discussion</a></li>
+               <li class="cat-item cat-item-5"><a href="https://www.nexmo.com/blog/category/events/">Events</a></li>
+               <li class="cat-item cat-item-38"><a href="https://www.nexmo.com/blog/category/giving-back/">Giving Back</a></li>
+               <li class="cat-item cat-item-197"><a href="https://www.nexmo.com/blog/category/hack-worthy/">Hack-Worthy</a></li>
+               <li class="cat-item cat-item-11"><a href="https://www.nexmo.com/blog/category/messaging/">Messaging</a></li>
+               <li class="cat-item cat-item-65"><a href="https://www.nexmo.com/blog/category/nexmo-news/">Nexmo News</a></li>
+               <li class="cat-item cat-item-170"><a href="https://www.nexmo.com/blog/category/number-insight/">Number Insight</a></li>
+               <li class="cat-item cat-item-50"><a href="https://www.nexmo.com/blog/category/partners/">Partners</a></li>
+               <li class="cat-item cat-item-185"><a href="https://www.nexmo.com/blog/category/product/">Product</a></li>
+               <li class="cat-item cat-item-29"><a href="https://www.nexmo.com/blog/category/security/">Security</a></li>
+               <li class="cat-item cat-item-320"><a href="https://www.nexmo.com/blog/category/sms/">SMS</a></li>
+               <li class="cat-item cat-item-526"><a href="https://www.nexmo.com/blog/category/developers-2/tutorial/">Tutorial</a></li>
+               <li class="cat-item cat-item-1"><a href="https://www.nexmo.com/blog/category/uncategorised/">Uncategorised</a></li>
+               <li class="cat-item cat-item-502"><a href="https://www.nexmo.com/blog/category/user-acquisition/">User Acquisition</a></li>
+               <li class="cat-item cat-item-35"><a href="https://www.nexmo.com/blog/category/verify/">Verify</a></li>
+               <li class="cat-item cat-item-62"><a href="https://www.nexmo.com/blog/category/voice/">Voice</a></li>
+            </ul>
+         </div>
+         <!-- Popular Post Widget -->
+         <div class="widget widget_popular">
+            <h2 class="widgettitle">Popular</h2>
+            <!-- WordPress Popular Posts Plugin v3.3.4 [PHP] [monthly] [views] [custom] -->
+            <ul class="wpp-list">
+               <li> <a href="https://www.nexmo.com/blog/2015/01/13/top-5-reasons-sms-isnt-delivered/" title="Top 5 Reasons Your SMS Isn’t Being Delivered" class="wpp-post-title" target="_self">Top 5 Reasons Your SMS Isn’t Being Delivered</a> </li>
+               <li> <a href="https://www.nexmo.com/blog/2017/03/09/phone-number-verification-a-stronger-alternative-to-traditional-passwords/" title="Phone Number Verification: A Stronger Alternative to Traditional Passwords" class="wpp-post-title" target="_self">Phone Number Verification: A Stronger Alternative to Traditional Passwords</a> </li>
+               <li> <a href="https://www.nexmo.com/blog/2015/09/23/sms-101-why-messages-fail-to-deliver/" title="SMS 101: Why Messages Fail to Deliver" class="wpp-post-title" target="_self">SMS 101: Why Messages Fail to Deliver</a> </li>
+            </ul>
+            <!-- End WordPress Popular Posts Plugin v3.3.4 -->
+         </div>
+         <!-- Search Widget -->
+         <div class="widget widget_top_cont">
+            <h2 class="widgettitle">Top Contributors</h2>
+            <ul>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/elishahoffman/"><span></span>Elisha Hoffman</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/srisri/"><span></span>Srivatsan Srinivasan</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/tjlytle/"><span></span>Tim Lytle</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/leggetter/"><span></span>Phil Leggetter</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/tonyjamous/"><span></span>Tony Jamous</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/tomomi/"><span></span>Tomomi Imura</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/charlescosta/"><span></span>Charles Costa</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/glenkunene/"><span></span>Glen Kunene</a> <br>
+               </li>
+               <li>
+                  <a href="https://www.nexmo.com/blog/author/jameswinter/"><span></span>James Winter</a> <br>
+               </li>
+            </ul>
+            <div style="clear:both;"></div>
+         </div>
+         <!-- Archives Widget -->
+         <div class="widget widget_archive">
+            <h2 class="widgettitle">Archives</h2>
+            <ul>
+               <li><a href="https://www.nexmo.com/blog/2017/04/">April 2017</a></li>
+               <li><a href="https://www.nexmo.com/blog/2017/03/">March 2017</a></li>
+               <li><a href="https://www.nexmo.com/blog/2017/02/">February 2017</a></li>
+               <li><a href="https://www.nexmo.com/blog/2017/01/">January 2017</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/12/">December 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/11/">November 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/10/">October 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/09/">September 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/08/">August 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/07/">July 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/06/">June 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/05/">May 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/04/">April 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/03/">March 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/02/">February 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2016/01/">January 2016</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/12/">December 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/11/">November 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/10/">October 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/09/">September 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/08/">August 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/07/">July 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/06/">June 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/05/">May 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/04/">April 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/03/">March 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/02/">February 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2015/01/">January 2015</a></li>
+               <li><a href="https://www.nexmo.com/blog/2014/12/">December 2014</a></li>
+               <li><a href="https://www.nexmo.com/blog/2014/11/">November 2014</a></li>
+            </ul>
+         </div>
+      </div>
+   </div>
+   <!-- /grid -->
+</div>
