81444fdf57d68b387eba4850a20677482353ba7d
TMA\tnson <tnson>
2017-04-26 09:41:05 +0700

    update content blog page

--- BE/wp-content/themes/nexmo/page-blog.php
+++ BE/wp-content/themes/nexmo/page-blog.php
@@ -1,4 +1,8 @@
-<?php get_header("blog"); ?>
+<?php
+  get_header("blog");
+  global $post;
+  while ( have_posts() ) : the_post();
+ ?>
 <main role="main">
    <section class="PageTitle PageTitle--blog has-gradient-bg has-white-text">
       <div class="PageTitle-content">
@@ -10,132 +14,44 @@
    <section>
       <section class="Blog">
          <?php get_sidebar('blog');?>
-         
+
          <div class="Blog-content Blog-archive">
             <!-- article -->
-            <article id="post-13366" class="post-13366 post type-post status-publish format-standard has-post-thumbnail hentry category-customers">
-               <!-- post thumbnail -->
-               <div class="Blog-thumbnail">
-                  <a href="https://www.nexmo.com/blog/2017/04/21/roadmap-helps-business-travellers-stay-connected-nexmo-sms-api/" title="Roadmap Helps Business Travellers Stay Connected with the Nexmo SMS API">
-                  <img src="https://www.nexmo.com/wp-content/uploads/2017/04/2017-04-18_1518.png" class="attachment-post size-post wp-post-image" alt="Nexmo Roadmap Case Study">                                </a>
-               </div>
-               <!-- /post thumbnail -->
-               <div class="Blog-summary has-image">
-                  <!-- post title -->
-                  <h4>
-                     <a href="https://www.nexmo.com/blog/2017/04/21/roadmap-helps-business-travellers-stay-connected-nexmo-sms-api/" title="Roadmap Helps Business Travellers Stay Connected with the Nexmo SMS API">Roadmap Helps Business Travellers Stay Connected with the Nexmo SMS API</a>
-                  </h4>
-                  <!-- /post title -->
-                  <!-- post details -->
-                  <span class="date">April 21, 2017</span>
-                  <span class="author">Published by <a href="https://www.nexmo.com/blog/author/charlescosta/" title="Posts by Charles Costa" rel="author">Charles Costa</a>                                // <a href="https://www.nexmo.com/blog/category/customers/" rel="tag">Customers</a></span>
-                  <span class="comments"></span>
-                  <!-- /post details -->
-                  <p>
-                     <!-- AddThis Sharing Buttons above -->Roadmap is a mobile tool for travelers designed to create happy travelers and happy travel managers. The customizable iOS and Android app allows travel agents, enterprise companies, and travel management companies to create a mobile-friendly overview of their entire trip and creates relevant and local suggestions to save travelers’ time, money and create a far […]<!-- AddThis Sharing Buttons below --> <a href="https://www.nexmo.com/blog/2017/04/21/roadmap-helps-business-travellers-stay-connected-nexmo-sms-api/">Read more »</a>
-                  </p>
-               </div>
-            </article>
-            <!-- /article -->
-            <!-- article -->
-            <article id="post-13195" class="post-13195 post type-post status-publish format-standard has-post-thumbnail hentry category-carriers category-messaging category-sms">
-               <!-- post thumbnail -->
-               <div class="Blog-thumbnail">
-                  <a href="https://www.nexmo.com/blog/2017/04/20/sms-shortcodes-what-you-need-to-know/" title="SMS Shortcodes, What You Need to Know">
-                  <img src="https://www.nexmo.com/wp-content/uploads/2017/01/Voice-Based-Critical-Alerts-1200x628.jpg" class="attachment-post size-post wp-post-image" alt="">                                </a>
-               </div>
-               <!-- /post thumbnail -->
-               <div class="Blog-summary has-image">
-                  <!-- post title -->
-                  <h4>
-                     <a href="https://www.nexmo.com/blog/2017/04/20/sms-shortcodes-what-you-need-to-know/" title="SMS Shortcodes, What You Need to Know">SMS Shortcodes, What You Need to Know</a>
-                  </h4>
-                  <!-- /post title -->
-                  <!-- post details -->
-                  <span class="date">April 20, 2017</span>
-                  <span class="author">Published by <a href="https://www.nexmo.com/blog/author/charlescosta/" title="Posts by Charles Costa" rel="author">Charles Costa</a>                                // <a href="https://www.nexmo.com/blog/category/carriers/" rel="tag">Carriers</a> // <a href="https://www.nexmo.com/blog/category/messaging/" rel="tag">Messaging</a> // <a href="https://www.nexmo.com/blog/category/sms/" rel="tag">SMS</a></span>
-                  <span class="comments"></span>
-                  <!-- /post details -->
-                  <p>
-                     <!-- AddThis Sharing Buttons above -->Any cell phone user has become familiar with promotional SMS messages from five or six-digit numbers during the past few years. These numbers are shorter than standard phone numbers because they’re designed for application to person (A2P) communications. If your business requires marketing to mobile phone users or sending them messages to get them to […]<!-- AddThis Sharing Buttons below --> <a href="https://www.nexmo.com/blog/2017/04/20/sms-shortcodes-what-you-need-to-know/">Read more »</a>
-                  </p>
-               </div>
-            </article>
-            <!-- /article -->
-            <!-- article -->
-            <article id="post-13342" class="post-13342 post type-post status-publish format-standard has-post-thumbnail hentry category-customers">
-               <!-- post thumbnail -->
-               <div class="Blog-thumbnail">
-                  <a href="https://www.nexmo.com/blog/2017/04/19/99-co-leverages-nexmo-apis-to-secure-app-registrations-and-increase-user-engagement/" title="99.co Leverages Nexmo APIs to Secure App Registrations and Increase User Engagement">
-                  <img src="https://www.nexmo.com/wp-content/uploads/2017/01/Notification-and-Engagement-1200x628.jpg" class="attachment-post size-post wp-post-image" alt="">                                </a>
-               </div>
-               <!-- /post thumbnail -->
-               <div class="Blog-summary has-image">
-                  <!-- post title -->
-                  <h4>
-                     <a href="https://www.nexmo.com/blog/2017/04/19/99-co-leverages-nexmo-apis-to-secure-app-registrations-and-increase-user-engagement/" title="99.co Leverages Nexmo APIs to Secure App Registrations and Increase User Engagement">99.co Leverages Nexmo APIs to Secure App Registrations and Increase User Engagement</a>
-                  </h4>
-                  <!-- /post title -->
-                  <!-- post details -->
-                  <span class="date">April 19, 2017</span>
-                  <span class="author">Published by <a href="https://www.nexmo.com/blog/author/charlescosta/" title="Posts by Charles Costa" rel="author">Charles Costa</a>                                // <a href="https://www.nexmo.com/blog/category/customers/" rel="tag">Customers</a></span>
-                  <span class="comments"></span>
-                  <!-- /post details -->
-                  <p>
-                     <!-- AddThis Sharing Buttons above -->Despite having a population of just 5.4 million, Singapore is one of the top 10 real estate cities in the world. To help alleviate the pain of finding a suitable property, Singapore’s fastest growing property portal 99.co provides renters, buyers, and property agents with a powerful map-based property search engine to provide a fast and […]<!-- AddThis Sharing Buttons below --> <a href="https://www.nexmo.com/blog/2017/04/19/99-co-leverages-nexmo-apis-to-secure-app-registrations-and-increase-user-engagement/">Read more »</a>
-                  </p>
-               </div>
-            </article>
-            <!-- /article -->
-            <!-- article -->
-            <article id="post-13294" class="post-13294 post type-post status-publish format-standard has-post-thumbnail hentry category-carriers category-cloud-communications category-voice">
-               <!-- post thumbnail -->
-               <div class="Blog-thumbnail">
-                  <a href="https://www.nexmo.com/blog/2017/04/18/a-history-of-telecommunications-how-telecoms-became-just-another-interface/" title="A History of Telecommunications: How Telecoms Became Just Another Interface">
-                  <img src="https://www.nexmo.com/wp-content/uploads/2017/04/timeline_nexmo.png" class="attachment-post size-post wp-post-image" alt="communications APIs">                                </a>
-               </div>
-               <!-- /post thumbnail -->
-               <div class="Blog-summary has-image">
-                  <!-- post title -->
-                  <h4>
-                     <a href="https://www.nexmo.com/blog/2017/04/18/a-history-of-telecommunications-how-telecoms-became-just-another-interface/" title="A History of Telecommunications: How Telecoms Became Just Another Interface">A History of Telecommunications: How Telecoms Became Just Another Interface</a>
-                  </h4>
-                  <!-- /post title -->
-                  <!-- post details -->
-                  <span class="date">April 18, 2017</span>
-                  <span class="author">Published by <a href="https://www.nexmo.com/blog/author/glenkunene/" title="Posts by Glen Kunene" rel="author">Glen Kunene</a>                                // <a href="https://www.nexmo.com/blog/category/carriers/" rel="tag">Carriers</a> // <a href="https://www.nexmo.com/blog/category/cloud-communications/" rel="tag">Cloud Communications</a> // <a href="https://www.nexmo.com/blog/category/voice/" rel="tag">Voice</a></span>
-                  <span class="comments"></span>
-                  <!-- /post details -->
-                  <p>
-                     <!-- AddThis Sharing Buttons above -->Scroll through our interactive timeline to learn how telecommunications went from an expensive monopoly service to a commodity that developers can integrate into everyday business applications through cloud communications APIs. Telecommunications has come a long way in just a few decades. Until the early 1980s, party lines—telephone loop circuits where multiple homes would share a […]<!-- AddThis Sharing Buttons below --> <a href="https://www.nexmo.com/blog/2017/04/18/a-history-of-telecommunications-how-telecoms-became-just-another-interface/">Read more »</a>
-                  </p>
-               </div>
-            </article>
-            <!-- /article -->
-            <!-- article -->
-            <article id="post-13264" class="post-13264 post type-post status-publish format-standard has-post-thumbnail hentry category-chat-app category-customer-support tag-ai tag-artificial-intelligence tag-call-centers tag-chatbots tag-contact-centers tag-customer-service tag-customer-support">
-               <!-- post thumbnail -->
-               <div class="Blog-thumbnail">
-                  <a href="https://www.nexmo.com/blog/2017/04/14/ai-powered-chatbots-customer-service/" title="AI-powered Chatbots Augment the Future of Customer Service">
-                  <img src="https://www.nexmo.com/wp-content/uploads/2017/04/al_callcenter.jpg" class="attachment-post size-post wp-post-image" alt="AI-powered chatbots can augment the future of customer service by anticipating customer needs and automating routine customer service tasks.">                                </a>
-               </div>
-               <!-- /post thumbnail -->
-               <div class="Blog-summary has-image">
-                  <!-- post title -->
-                  <h4>
-                     <a href="https://www.nexmo.com/blog/2017/04/14/ai-powered-chatbots-customer-service/" title="AI-powered Chatbots Augment the Future of Customer Service">AI-powered Chatbots Augment the Future of Customer Service</a>
-                  </h4>
-                  <!-- /post title -->
-                  <!-- post details -->
-                  <span class="date">April 14, 2017</span>
-                  <span class="author">Published by <a href="https://www.nexmo.com/blog/author/derek-handova/" title="Posts by Derek Handova" rel="author">Derek Handova</a>                                // <a href="https://www.nexmo.com/blog/category/chat-app/" rel="tag">Chat App</a> // <a href="https://www.nexmo.com/blog/category/customer-support/" rel="tag">Customer Support</a></span>
-                  <span class="comments"></span>
-                  <!-- /post details -->
-                  <p>
-                     <!-- AddThis Sharing Buttons above -->Fully cognitive artificial intelligence (AI) is not here yet, but something that feels like it is here now and already helping businesses create better customer experiences. That something is AI-powered chatbots. This was one of the key takeaways from an April 13 VentureBeat webinar titled&nbsp;Customer Support: Unleash Your Superpowers with AI. Stewart Rogers, VentureBeat director […]<!-- AddThis Sharing Buttons below --> <a href="https://www.nexmo.com/blog/2017/04/14/ai-powered-chatbots-customer-service/">Read more »</a>
-                  </p>
-               </div>
-            </article>
+
+            <?php $loop = new WP_Query( array( 'post_type' => 'blog', 'posts_per_page' => -1 ) ); ?>
+              <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                <article id="post-13366" class="post-13366 post type-post status-publish format-standard has-post-thumbnail hentry category-customers">
+                   <!-- post thumbnail -->
+                   <div class="Blog-thumbnail">
+                      <a href="<?php echo get_permalink( $post->ID ); ?>" title="<?php echo get_the_title(); ?>">
+                        <?php
+                          $image = get_field('image_single_blog');
+                          if( !empty($image) ): ?>
+                        	<img src="<?php echo $image['url']; ?>?>" class="attachment-post size-post wp-post-image" alt="<?php echo $image['alt']; ?>">
+                        <?php endif; ?>
+                      </a>
+                   </div>
+                   <!-- /post thumbnail -->
+                   <div class="Blog-summary has-image">
+                      <!-- post title -->
+                      <h4>
+                         <a href="<?php echo get_permalink( $post->ID ); ?>" title="<?php echo get_the_title(); ?>"><?php echo get_the_title(); ?></a>
+                      </h4>
+                      <!-- /post title -->
+                      <!-- post details -->
+                      <span class="date"><?php the_date(); ?></span>
+                      <span class="author">Published by <a href="<?php the_author_link();?>" title="Posts by Charles Costa" rel="author"><?php the_author(); ?></a>
+                                                        // <a href="https://www.nexmo.com/blog/category/customers/" rel="tag">Customers</a></span>
+                      <span class="comments"></span>
+                      <!-- /post details -->
+                      <p>
+                         <!-- AddThis Sharing Buttons above --><?php echo wp_trim_words( get_the_content(), 55, '[…]' );?><!-- AddThis Sharing Buttons below -->
+                         <a href="<?php echo get_permalink( $post->ID ); ?>">Read more »</a>
+                      </p>
+                   </div>
+                </article>
+              <?php endwhile; wp_reset_query(); ?>
+
             <!-- /article -->
             <!-- pagination -->
             <div class="pagination">
@@ -152,4 +68,5 @@
    </section>
    <!-- /section -->
 </main>
+<?php endwhile; ?>
 <?php get_footer();?>
